1796a677901ce9865793056caef9ad6071b8e287
mhadih <m.hadi.hojjat@gmail.com>
2021-10-19 18:39:25 +0330

    question2b - fix bug

--- src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
@@ -1,7 +1,7 @@
 package org.springframework.samples.petclinic.owner;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -14,18 +14,39 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assume.assumeTrue;
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(Parameterized.class)
-class PetServiceTest {
-	private PetService petService;
+public class PetServiceTest {
 	private Integer petId;
+	private PetService petService;
 
 	private final Logger loggerMock = Mockito.mock(Logger.class);
 
 	public PetServiceTest(Integer petId) {
 		this.petId = petId;
+	}
+
+	static class FakePetTimedCache extends PetTimedCache {
+		private final Map<Integer, Pet> pets;
+
+		public FakePetTimedCache() {
+			super(null);
+			pets = new HashMap<>();
+			for (int i = 0; i < 10; i++) {
+				Pet pet = new Pet();
+				pet.setId(i);
+				pet.setName("pet" + i);
+				pets.put(i, pet);
+			}
+		}
+
+		@Override
+		public Pet get(Integer petId) {
+			return pets.get(petId);
+		}
 	}
 
 	@Before
@@ -48,23 +69,4 @@ class PetServiceTest {
 		assertEquals(pet.getId(), petId);
 	}
 
-	static class FakePetTimedCache extends PetTimedCache {
-		private final Map<Integer, Pet> pets;
-
-		public FakePetTimedCache() {
-			super(null);
-			pets = new HashMap<>();
-			for (int i = 0; i < 10; i++) {
-				Pet pet = new Pet();
-				pet.setId(i);
-				pet.setName("pet" + i);
-				pets.put(i, pet);
-			}
-		}
-
-		@Override
-		public Pet get(Integer petId) {
-			return pets.get(petId);
-		}
-	}
 }
